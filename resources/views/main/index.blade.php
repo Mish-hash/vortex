ed1cc3f7070030de3270c852871f9b4e7017f1ba
Mish-hash <poluektov23@gmail.com>
2020-08-07 19:49:40 +0300

    added homepage and main layout

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,10 +1,26 @@
-<h1>{{$title}}</h1>
+@extends('main.layout')
 
-@foreach ($categories as $category)
-    <h3>{{$category}}</h3><br>
+@section('title', $title)
 
-@endforeach
-{{-- Шаблон коментария --}}
-@isset($n)
-    <h3>{{$n}}</h3>
-@endisset
+
+@section('content')
+        
+
+    <h1>{{$title}}</h1>
+
+    @foreach ($categories as $category)
+        <h3>{{$category}}</h3><br>
+
+    @endforeach
+    {{-- Шаблон коментария --}}
+    @isset($n)
+        <h3>{{$n}}</h3>
+    @endisset
+
+
+@endsection
+
+@section('footer')
+    @parent <br>
+    Реклама
+@endsection
--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
 </head>
 <body>
     <header>Некий хедер</header>
@@ -12,6 +12,10 @@
         <h3>Рабочий div</h3>
         @yield('content')
     </div>
-    <footer>Некий футер</footer>
+    <footer>
+        @section('footer')
+            Некое меню
+        @show
+    </footer>
 </body>
 </html>
