3f8ee194eb13c021a58877f1ad22f41fce71ade2
Mish-hash <poluektov23@gmail.com>
2020-08-18 20:02:03 +0300

    modified page product

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -12,20 +12,22 @@
     <p>{{$product->description}}</p>
     <p>Price: {{$product->price}}</p>
     
-    
-    <div class="row">
-        @foreach ($crosselProducts as $crosselProduct)
-            <div class="col-md-4 mb-5">
-                <p>crosselProduct</p>
-                <a href="/product/{{$crosselProduct->slug}}">
-                    <img src="{{$crosselProduct->img_url}}" alt="{{$crosselProduct->name}}" class="img-fluid">
-                    <h3>{{$crosselProduct->name}}</h3>
-                    <p>Price: {{$crosselProduct->price}}</p>
-                </a>
-            </div>
-        @endforeach
-    </div>
-    
+    @isset($crosselProducts)
+
+        <div class="row">
+            @foreach ($crosselProducts as $crosselProduct)
+                <div class="col-md-4 mb-5">
+                    <p>crosselProduct</p>
+                    <a href="/product/{{$crosselProduct->slug}}">
+                        <img src="{{$crosselProduct->img_url}}" alt="{{$crosselProduct->name}}" class="img-fluid">
+                        <h3>{{$crosselProduct->name}}</h3>
+                        <p>Price: {{$crosselProduct->price}}</p>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        
+    @endisset
 
 @endsection
 
